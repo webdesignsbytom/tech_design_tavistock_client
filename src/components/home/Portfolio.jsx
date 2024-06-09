3877b61a117fd309f99df4e788fdeb26f4eabbbe
xtombrock <xtombrock1989@gmail.com>
2024-06-09 20:52:37 +0100

    save

--- src/components/home/Portfolio.jsx
+++ src/components/home/Portfolio.jsx
@@ -10,44 +10,47 @@ import {
 import ButtonComponent from '../global/ButtonComponent';
 
 function Portfolio() {
+  const articleRefs = useRef([]);
+  const sectionRef = useRef(null); // Reference for the section
   const [portfolioImages, setPortfolioImages] = useState(
     portfolioWebImagesArray
   );
-  const [displayType, setDisplayType] = useState('web'); // Add this state to keep track of the display type
+  const [displayType, setDisplayType] = useState('web'); // Track of the display type
+  const [allowAnimation, setAllowAnimation] = useState(true); // Track of the display type
   let navigate = useNavigate();
-  const articleRefs = useRef([]);
-  const sectionRef = useRef(null); // Reference for the section
 
   useEffect(() => {
-    const handleIntersection = (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          const imgContainer = entry.target.querySelector('.image-container');
-          if (imgContainer) {
-            // Check if imgContainer is not null
-            imgContainer.classList.add('start-scrolling');
-            imgContainer.scrollTo({
-              top: imgContainer.scrollHeight,
-              behavior: 'smooth',
-            });
+    if (allowAnimation) {
+      const handleIntersection = (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const imgContainer = entry.target.querySelector('.image-container');
+            if (imgContainer) {
+              // Check if imgContainer is not null
+              imgContainer.classList.add('start-scrolling');
+              imgContainer.scrollTo({
+                top: imgContainer.scrollHeight,
+                behavior: 'smooth',
+              });
+            }
           }
-        }
+        });
+      };
+
+      const observer = new IntersectionObserver(handleIntersection, {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5,
       });
-    };
 
-    const observer = new IntersectionObserver(handleIntersection, {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.5,
-    });
+      articleRefs.current.forEach((article) => {
+        if (article) observer.observe(article);
+      });
 
-    articleRefs.current.forEach((article) => {
-      if (article) observer.observe(article);
-    });
-
-    return () => {
-      observer.disconnect();
-    };
+      return () => {
+        observer.disconnect();
+      };
+    }
   }, []);
 
   const navigateToPage = (page) => {
@@ -106,7 +109,7 @@ function Portfolio() {
 
   return (
     <section
-    ref={sectionRef}
+      ref={sectionRef}
       id='portfolio-section'
       className='grid relative h-full min-h-screen lg:max-h-screen lg:overflow-hidden mt-10'
     >
