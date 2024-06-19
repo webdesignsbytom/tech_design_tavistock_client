ce02f9c2988b76dc831c3f24ffec45cf2f5e421d
xtombrock <xtombrock1989@gmail.com>
2024-06-19 20:54:38 +0100

    save

--- src/utils/ReviewsDataUtil.jsx
+++ src/utils/ReviewsDataUtil.jsx
@@ -11,28 +11,28 @@ export const reviewsDataArray = [
         id: reviewId++,
         title: 'Wervings',
         desc: 'Tom has run our business ecommerce website for years now and has always done splendid work! Very easy to work with.',
-        url: 'www.wervings.co.uk',
+        url: 'https://www.wervings.co.uk',
         imageUrl: WervingsLogo
     },
     {
         id: reviewId++,
         title: 'Rapidvansolutions',
         desc: 'Thank you for designing and building our website. I understand nothing about the internet and Tom worked with that.',
-        url: 'www.rapidvansolutions.com',
+        url: 'https://www.rapidvansolutions.com',
         imageUrl: RVSLogo
     },
     {
         id: reviewId++,
         title: 'Stuart Turnbull',
         desc: 'A hard working man and a pleasure to work with. Said yes to everything i asked and didnt over complicate the job.',
-        url: 'www.stuartturnbull.net',
+        url: 'https://www.stuartturnbull.net',
         imageUrl: MusicLogo
     },
     {
         id: reviewId++,
         title: 'Myecoapp.org',
         desc: 'Tom built a beautiful site for us and brings most of the ideas about improvements and modernisations.',
-        url: 'www.myecoapp.org',
+        url: 'https://www.myecoapp.org',
         imageUrl: MyeaLogo
     }
 ]
